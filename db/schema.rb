a8cbb02100c89fb570a4d4549962b97b5cc80336
tol3 <tol3@me.com>
2014-05-31 20:15:42 +0700

    tob commit

--- db/schema.rb
+++ db/schema.rb
@@ -98,12 +98,6 @@ ActiveRecord::Schema.define(:version => 20140525173328) do
     t.boolean  "publish"
   end
 
-  create_table "types", :force => true do |t|
-    t.string   "type"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password"
